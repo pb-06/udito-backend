58822a622622b38a363e787b28a7f3325569173d
pb-06 <kkandomail@gmail.com>
2025-09-18 08:54:32 +0200

    Respond updated

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -118,6 +118,82 @@ app.post('/udito/:id', (req, res) => {
     res.status(201).json(responseBody);
 });
 
+app.patch('/udito/:id', (req, res) => {
+    const data = fs.readFileSync('uditok.txt');
+    const responseBodyArr = uditoDataRead(data);
+    console.log('responseBodyArr before', responseBodyArr);
+    console.log('params', req.params);
+    const id = +req.params.id;
+    console.log('id', id);
+
+    const foundIndex = responseBodyArr.findIndex(udito => +udito.id == +id);
+    console.log('foundIndex', foundIndex);
+    if (foundIndex < 0) {
+        res.status(400).json({error: 'id not found'});
+    } else {
+        console.log('req.body', req.body);
+        const responseBody = {id, ...req.body};
+        console.log('responseBody', responseBody);
+
+        responseBodyArr.splice(foundIndex, 1, responseBody);
+        console.log('responseBodyArr after splice', responseBodyArr);
+    }
+
+    // TODO - respond actual
+    res.status(200).json(responseBodyArr);
+})
+
+app.delete('/udito/:id', (req, res) => {
+    console.log('params', req.params);
+    // req.body json: {"nev":"Sprite","liter":1,"bubis-e":true}
+
+    // if id exists?
+    let existingId;
+    fs.readFile('uditok.txt', (err, data) => {
+        // TODO - make readFile() sync
+        if (err) {
+            res.status(404).json({ fileError: err });
+        }
+        else {
+            if (data) {
+                const responseBodyArr = uditoDataRead(data);
+
+                console.log('params', req.params);
+                const id = +req.params?.id;
+
+                const foundIndex = responseBodyArr.findIndex(udito => +udito.id == +id);
+                console.log('foundIndex', foundIndex);
+
+                if (foundIndex >= 0) {
+                    existingId = id;
+                    //res.status(300).json({ error: "existing id conflict" }); // kliens oldali hiba, a kliens adott meg rossz ID-t
+                }
+            }
+            //else {
+            //    res.status(404).json({ fileError: data }); // This is not a bug, but a feature!
+            //}
+        }
+    });
+
+    // if id does not exist yet:
+    const id = +req.params.id;
+
+    const deletableFileLine = `${req.params?.id};${req.body.nev};${req.body.liter};${req.body["bubis-e"]}`;
+    try {
+        // TODO - delete id line!
+        console.log('id, data', id, data);
+        fs.writeFileSync('udito.txt', data);
+    } catch (e) {
+        res.status(500).json({ fileError: e });
+    }
+
+    const responseBody = { id, ...req.body };
+
+    console.log('existingId', existingId);
+
+    res.status(201).json(responseBody);
+})
+
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'error.html'));
 });
